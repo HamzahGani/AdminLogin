076985d8bc4f4eab0bf4bf7c8ea606372129c3b7
HGani <hgani786@gmail.com>
2021-08-24 17:01:41 +0200

    Building Profile Page

--- AdminLogin/AdminLoginForm.cs
+++ AdminLogin/AdminLoginForm.cs
@@ -26,115 +26,8 @@ namespace AdminLogin
             
         }
 
-        private void btnLogin_Click(object sender, EventArgs e)
-        {
-            lblError.Visible = false;
-            using (SqlConnection sqlCon = new SqlConnection(connectionString))
-            {
-                sqlCon.Open();
-                SqlCommand sqlCom = new SqlCommand();
-                sqlCom.Connection = sqlCon;
-                sqlCom.CommandText = "SELECT * FROM Users WHERE Username ='" + txtUser.Text + "' and Password ='" + txtPass.Text + "'";
-                SqlDataReader sqlDR = sqlCom.ExecuteReader();
-                if (sqlDR.Read())
-                {
-                    //if(txtUser.Text.Equals(sqlDR["username"].ToString()) && txtPass.Text.Equals(sqlDR["password"].ToString()))
-                    //if (txtPass.Text.Equals(sqlDR["password"].ToString()))
-                        //{
-                        loggedUser = txtUser.Text;
-                        txtPass.Text = "";
-                        loadPage("Menu");
-                    //}
-                    
-                }
-                else
-                {
-                    lblError.Text = "Username or password is incorrect";
-                    lblError.Visible = true;
-                    //MessageBox.Show("Username or password is incorrect", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-
-                sqlCon.Close();
-            }
-        }
-
         private void pnlLogin_Paint(object sender, PaintEventArgs e)
         {
-        }
-
-        private void btnUserPage_Click(object sender, EventArgs e)
-        {
-            loadPage("Users");
-        }
-
-        private void btnProfilePage_Click(object sender, EventArgs e)
-        {
-            loadPage("Profile");
-            string profileSearch = "SELECT Username, Name, Surname, Number " +
-                "FROM Users WHERE Username ='" + loggedUser + "'";
-
-            using (SqlConnection sqlCon = new SqlConnection(connectionString))
-            {
-                sqlCon.Open();
-                SqlCommand sqlCom = new SqlCommand();
-                sqlCom.Connection = sqlCon;
-                sqlCom.CommandText = profileSearch;
-                SqlDataReader sqlDR = sqlCom.ExecuteReader();
-                if (sqlDR.Read())
-                {
-                    txtProfileUsername.Text = sqlDR["Username"].ToString();
-                    txtProfileName.Text = sqlDR["Name"].ToString();
-                    txtProfileSurname.Text = sqlDR["Surname"].ToString();
-                    txtProfileNumber.Text = sqlDR["Number"].ToString();
-
-                }
-                else
-                {
-                    lblError.Text = "Cannot find username";
-                    lblError.Visible = true;
-                }
-
-                sqlCon.Close();
-            }
-        }
-
-        private void btnLogOut_Click(object sender, EventArgs e)
-        {
-            loadPage("Login");
-        }
-
-        private void btnSearch_Click(object sender, EventArgs e)
-        {
-            string emptyUserSearch = "SELECT Username, IsAdmin, Name, Surname, Number FROM Users";
-            string userSearch = "SELECT Username, IsAdmin, Name, Surname, Number " +
-                "FROM Users WHERE Username ='" + txtSearch.Text +
-                "' or Name ='" + txtSearch.Text +
-                "' or Surname ='" + txtSearch.Text +
-                "' or Number ='" + txtSearch.Text + "'";
-
-            using (SqlConnection sqlCon = new SqlConnection(connectionString))
-            {
-                sqlCon.Open();
-                DataTable sqlDT = new DataTable();
-                if (txtSearch.Text == "")
-                {
-                    SqlDataAdapter sqlDA = new SqlDataAdapter(emptyUserSearch, sqlCon);
-                    sqlDA.Fill(sqlDT);
-                }
-                else
-                {
-                    SqlDataAdapter sqlDA = new SqlDataAdapter(userSearch, sqlCon);
-                    sqlDA.Fill(sqlDT);
-                }
-                dgvUsers.DataSource = sqlDT;
-                sqlCon.Close();
-            }
-        }
-
-        private void btnMenuFromUsers_Click(object sender, EventArgs e)
-        {
-            txtSearch.Text = "";
-            loadPage("Menu");
         }
 
         private void loadPage(string newPage)
@@ -176,10 +69,105 @@ namespace AdminLogin
             }
         }
 
-        private void btnMenuFromProfile_Click(object sender, EventArgs e)
+        private void btnLogin_Click(object sender, EventArgs e)
         {
-            lockProfilePage();
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                SqlCommand sqlCom = new SqlCommand();
+                sqlCom.Connection = sqlCon;
+                sqlCom.CommandText = "SELECT * FROM Users WHERE Username ='" + txtUser.Text + "' and Password ='" + txtPass.Text + "'";
+                SqlDataReader sqlDR = sqlCom.ExecuteReader();
+                if (sqlDR.Read())
+                {
+                        loggedUser = txtUser.Text;
+                        txtPass.Text = "";
+                        loadPage("Menu");
+                    
+                }
+                else
+                {
+                    lblError.Text = "Username or password is incorrect";
+                    lblError.Visible = true;
+                }
+
+                sqlCon.Close();
+            }
+        }
+
+        private void btnLogOut_Click(object sender, EventArgs e)
+        {
+            loadPage("Login");
+        }
+
+        private void btnUserPage_Click(object sender, EventArgs e)
+        {
+            loadPage("Users");
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string emptyUserSearch = "SELECT Username, IsAdmin, Name, Surname, Number FROM Users";
+            string userSearch = "SELECT Username, IsAdmin, Name, Surname, Number " +
+                "FROM Users WHERE Username ='" + txtSearch.Text +
+                "' or Name ='" + txtSearch.Text +
+                "' or Surname ='" + txtSearch.Text +
+                "' or Number ='" + txtSearch.Text + "'";
+
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                DataTable sqlDT = new DataTable();
+                if (txtSearch.Text == "")
+                {
+                    SqlDataAdapter sqlDA = new SqlDataAdapter(emptyUserSearch, sqlCon);
+                    sqlDA.Fill(sqlDT);
+                }
+                else
+                {
+                    SqlDataAdapter sqlDA = new SqlDataAdapter(userSearch, sqlCon);
+                    sqlDA.Fill(sqlDT);
+                }
+                dgvUsers.DataSource = sqlDT;
+                sqlCon.Close();
+            }
+        }
+
+        private void btnMenuFromUsers_Click(object sender, EventArgs e)
+        {
+            txtSearch.Text = "";
             loadPage("Menu");
+        }
+
+        private void btnProfilePage_Click(object sender, EventArgs e)
+        {
+            loadPage("Profile");
+            string profileSearch = "SELECT Username, Name, Surname, Number " +
+                "FROM Users WHERE Username ='" + loggedUser + "'";
+
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                SqlCommand sqlCom = new SqlCommand();
+                sqlCom.Connection = sqlCon;
+                sqlCom.CommandText = profileSearch;
+                SqlDataReader sqlDR = sqlCom.ExecuteReader();
+                if (sqlDR.Read())
+                {
+                    txtProfileUsername.Text = sqlDR["Username"].ToString();
+                    txtProfileName.Text = sqlDR["Name"].ToString();
+                    txtProfileSurname.Text = sqlDR["Surname"].ToString();
+                    txtProfileNumber.Text = sqlDR["Number"].ToString();
+
+                }
+                else
+                {
+                    lblError.Text = "Cannot find username";
+                    lblError.Visible = true;
+                }
+
+                sqlCon.Close();
+            }
         }
 
         private void btnProfileEdit_Click(object sender, EventArgs e)
@@ -198,11 +186,47 @@ namespace AdminLogin
         private void btnProfileUpdate_Click(object sender, EventArgs e)
         {
             lockProfilePage();
+            //update db
+
+             /*
+            string updateProfile = "UPDATE Users Set Username, Name, Surname, Number " +
+                "FROM Users WHERE Username ='" + loggedUser + "'";
+
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                SqlCommand sqlCom = new SqlCommand();
+                sqlCom.Connection = sqlCon;
+                sqlCom.CommandText = updateProfile;
+                SqlDataReader sqlDR = sqlCom.ExecuteReader();
+                if (sqlDR.Read())
+                {
+                    txtProfileUsername.Text = sqlDR["Username"].ToString();
+                    txtProfileName.Text = sqlDR["Name"].ToString();
+                    txtProfileSurname.Text = sqlDR["Surname"].ToString();
+                    txtProfileNumber.Text = sqlDR["Number"].ToString();
+
+                }
+                else
+                {
+                    lblError.Text = "Cannot find username";
+                    lblError.Visible = true;
+                }
+
+                sqlCon.Close();
+            }
+            // */
         }
 
         private void btnCancelProfileUpdate_Click(object sender, EventArgs e)
         {
             lockProfilePage();
+        }  
+
+        private void btnMenuFromProfile_Click(object sender, EventArgs e)
+        {
+            lockProfilePage();
+            loadPage("Menu");
         }
 
         private void lockProfilePage()
